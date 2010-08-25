8380c2b22bb2e7ecd1b96c5b6241e1e90bad31aa
Sean Wild <saw792@hotmail.com>
2010-08-25 15:31:11 +1000

    Added Vic's code

--- src/LapTimer.java
+++ src/LapTimer.java
@@ -1,9 +1,68 @@
-import java.util.ArrayList;
+/*  LapTimer
+ * 
+ *  25/8/2010
+ * 
+ *  Victoria Hone
+ * 
+ *  v1.0.0
+ * 
+ *  This class generates a lap timer to display on the screen
+ *  while the game is running. It provides functions to get the
+ *  current lap time, the lap time of the previous race and
+ *  format the time for display on the screen.
+ * 
+ */
+
+
+import java.util.Calendar;
 
 public class LapTimer extends UIObject {
-	//THREAD: GRAPHICS/UI
-	
-	String time_display;
-	ArrayList<ArrayList<Float>> laptimes;
-	ArrayList<Boolean> stop_timing;
+    //THREAD: GRAPHICS/UI
+    private static float width = 200;
+    private static float height = 100;
+    private static float xpos = 300;
+    private static float ypos = 300;
+   
+    private static long startTime = 0;
+    private static long stopTime = 0;
+   
+   
+    private boolean running = false;
+       
+    public LapTimer() {
+        super(width, height, xpos, ypos);
+    }
+       
+    public long getTime() {
+        if (running){
+            long currentTime = Calendar.getInstance().getTimeInMillis();
+            return currentTime - startTime;
+        }else {
+            return stopTime - startTime;
+        }
+    }
+   
+    public void start() {
+        running = true;
+        startTime = Calendar.getInstance().getTimeInMillis();
+    }
+       
+    public void stop() {
+        running = false;
+        stopTime = Calendar.getInstance().getTimeInMillis();
+    }
+   
+    public String formatTime(long time){
+        long min = time/60000;
+        long remainder = time%60000;
+        long sec = remainder/1000;
+        long tenths = (remainder%1000)/10;
+        return (min + ":" + sec + ":" + tenths);
+    }
+   
+    public static void main(String[] args){
+        LapTimer timer = new LapTimer();
+        System.out.println(timer.formatTime(timer.getTime()));
+    }
+   
 }
--- src/UIObject.java
+++ src/UIObject.java
@@ -1,8 +1,49 @@
-
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.geom.RoundRectangle2D;
+ 
+import javax.swing.JFrame;
+ 
+ 
 public class UIObject {
-  //THREAD: GRAPHICS/UI
-	
-//2D user interface component
-//Holds a certain xy position on the screen (opengl coords not track coords)
-//Has a certain width and height
+    //THREAD: GRAPHICS/UI
+     
+    //2D user interface component
+    //Holds a certain xy position on the screen (opengl coords not track coords)
+    //Has a certain width and height
+         
+    private float x, y;
+    private float w, h; 
+    private static Graphics graphics;
+    private Canvas canvas;
+         
+    public UIObject(/*LGraphics gl,*/ float width, float height, float xpos, float ypos) {
+        JFrame frame = new JFrame();
+        canvas = new Canvas();
+        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        frame.add(canvas);
+        frame.setVisible(true);
+        graphics = frame.getGraphics();//gl.getGraphics2D();
+        w = width;
+        h = height;
+        x = xpos;
+        y = ypos;
+        //RoundRectangle2D ugly = new RoundRectangle2D.Float(5, 5, x, y, w, h);
+        canvas.paint(graphics);
+        graphics.drawRoundRect((int)x,(int) y,(int) width, (int)height, 5, 5);
+    }
+         
+    public void addRectangle() {
+         
+        RoundRectangle2D ugly = new RoundRectangle2D.Float(5, 5, x, y, w, h);
+    }
+         
+    public void addCircle() {
+             
+    }
+         
+    public void update() {
+        canvas.update(graphics);
+    }
 }
