e6d4538a8fba77f160a5c533d2b186fbdf9d42c8
Sean Wild <saw792@hotmail.com>
2010-08-27 01:53:00 +1000

    fix merge

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,4 +4,8 @@ public class MainMenu extends UIObject {
 	
   //Action and event responses for button clicking
   //Events should be passed through from graphics library
+	
+	public MainMenu() {
+		super(0, 0, 0, 0);
+	}
 }
